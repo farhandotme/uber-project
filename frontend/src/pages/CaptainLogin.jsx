9c595b175b3ebd1c72052b0eee4ced6b26d3d9cb
farhandotme <princeofficial355@gmail.com>
2025-02-11 15:33:07 +0530

    done captain registration and login - frontend

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,6 +1,84 @@
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash, FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const CaptainLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+
+  }
+
   return (
-    <div>CaptainLogin</div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <div className="flex flex-col items-center justify-center">
+          <img
+            className="w-30 mb-6"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaI0-AaIAcwVCkcnR8xdetso-wz9rCOVJB5Q&s"
+            alt="Uber Logo"
+          />
+          <h1 className="text-2xl text-[#10b461] font-bold">You are Logging in as captain</h1>
+          <form onSubmit={(e) => { handleSubmit(e) }} className="flex flex-col m-6 w-full max-w-md p-6 bg-white rounded-lg">
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="email">
+              What&apos;s your email?
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="bg-gray-200 p-2 h-12 rounded"
+              type="text"
+              name="email"
+              placeholder="email@example.com"
+            />
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="password">
+              Enter password
+            </label>
+            <div className="relative">
+              <input
+                className="bg-gray-200 p-2 h-12 rounded w-full pr-10"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 flex items-center pr-3"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+              </button>
+            </div>
+            <button className="bg-[#10b461] flex justify-center items-center gap-5 text-white p-2 h-12 rounded mt-5 text-xl">
+              Login
+              <FaArrowRight />
+            </button>
+          </form>
+          <div>
+            <p className="text-center">
+              Don&apos;t have an account?{" "}
+              <Link to="/captain-register" className="text-blue-500">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-6">
+        <Link to={"/user-login"}><button className="bg-black w-85 h-15 text-white p-2 rounded text-xl">
+          Login as User
+        </button></Link>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/CaptainRegister.jsx
+++ frontend/src/pages/CaptainRegister.jsx
@@ -1,6 +1,159 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaRegEye, FaRegEyeSlash, FaArrowRight } from "react-icons/fa";
 
 export const CaptainRegister = () => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [plate, setPlate] = useState('');
+  const [color, setColor] = useState('');
+  const [model, setModel] = useState('');
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log({
+      fullname: {
+        firstname,
+        lastname
+      }, email, password, vehicle: {
+        plate, color, model
+      }
+    });
+  };
+
   return (
-    <div>CaptainRegister</div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <div className="flex flex-col items-center justify-center">
+          <img
+            className="w-30 mb-6"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaI0-AaIAcwVCkcnR8xdetso-wz9rCOVJB5Q&s"
+            alt="Uber Logo"
+          />
+          <h1 className="text-2xl text-[#10b461] font-bold">You are registering as captain</h1>
+
+          <form className="flex flex-col m-6 w-full max-w-md p-6 bg-white rounded-lg" onSubmit={handleRegister}>
+            <div className='flex justify-between gap-2'>
+              <div className="flex-1">
+                <label className="text-xl font-bold mt-5 mb-2" htmlFor="firstname">
+                  First Name
+                </label>
+                <input
+                  onChange={(e) => setFirstname(e.target.value)}
+                  value={firstname}
+                  className="bg-gray-200 p-2 h-12 rounded w-full"
+                  type="text"
+                  name="firstname"
+                  placeholder="First name"
+                />
+              </div>
+              <div className="flex-1">
+                <label className="text-xl font-bold mt-5 mb-2" htmlFor="lastname">
+                  Last Name
+                </label>
+                <input
+                  onChange={(e) => setLastname(e.target.value)}
+                  value={lastname}
+                  className="bg-gray-200 p-2 h-12 rounded w-full"
+                  type="text"
+                  name="lastname"
+                  placeholder="Last name"
+                />
+              </div>
+            </div>
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="bg-gray-200 p-2 h-12 rounded"
+              type="email"
+              name="email"
+              placeholder="email@example.com"
+            />
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="password">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                className="bg-gray-200 p-2 h-12 rounded w-full pr-10"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 flex items-center pr-3"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+              </button>
+            </div>
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="plate">
+              Plate
+            </label>
+            <input
+              onChange={(e) => setPlate(e.target.value)}
+              value={plate}
+              className="bg-gray-200 p-2 h-12 rounded"
+              type="text"
+              name="plate"
+              placeholder="Plate"
+            />
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="color">
+              Color
+            </label>
+            <input
+              onChange={(e) => setColor(e.target.value)}
+              value={color}
+              className="bg-gray-200 p-2 h-12 rounded"
+              type="text"
+              name="color"
+              placeholder="Color"
+            />
+            <label className="text-xl font-bold mt-5 mb-2" htmlFor="model">
+              Model
+            </label>
+            <input
+              onChange={(e) => setModel(e.target.value)}
+              value={model}
+              className="bg-gray-200 p-2 h-12 rounded"
+              type="text"
+              name="model"
+              placeholder="Model"
+            />
+            <button className="bg-[#10b461] flex justify-center items-center gap-5 text-white p-2 h-12 rounded mt-5 text-xl">
+              Register <FaArrowRight />
+            </button>
+          </form>
+          <div>
+            <p className="text-center">
+              Already have an account?{' '}
+              <Link to="/captain-login" className="text-blue-500">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-6">
+        <Link to="/user-register">
+          <button className="bg-black w-85 h-15 text-white p-2 rounded text-xl">
+            Register as User
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
